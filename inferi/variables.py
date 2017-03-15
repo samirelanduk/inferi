8424546dd332f26bf9c1402454b62ab055ee54b5
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-15 11:00:25 +0000

    Add Variable name

--- inferi/variables.py
+++ inferi/variables.py
@@ -1,4 +1,8 @@
 class Variable(list):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, name=None, **kwargs):
         list.__init__(self, [*args], **kwargs)
+
+        if not isinstance(name, str) and name is not None:
+            raise TypeError("Variable name must be str, not '%s'" % str(name))
+        self._name = name
--- tests/test_variables.py
+++ tests/test_variables.py
@@ -6,3 +6,13 @@ class VariableCreationTests(TestCase):
     def test_can_create_variable(self):
         var = Variable(11, 45, 23, 12, 9)
         self.assertIsInstance(var, list)
+
+
+    def test_can_create_variable_with_name(self):
+        var = Variable(11, 45, 23, 12, 9, name="heights")
+        self.assertEqual(var._name, "heights")
+
+
+    def test_name_must_be_str(self):
+        with self.assertRaises(TypeError):
+            Variable(11, 45, 23, 12, 9, name=100)
