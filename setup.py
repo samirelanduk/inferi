5a18644b9e0a65e85d93c9bd24d1d75b1fbedf15
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-15 10:44:15 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name="inferi",
+ version="0.1.0",
+ description="A statistics library.",
+ url="https://inferi.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Mathematics",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+ ],
+ keywords="statistics probability data-science",
+ packages=["inferi"]
+)
