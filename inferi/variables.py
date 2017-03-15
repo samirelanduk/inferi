f17acac478dd7077db3ac925786d230e6f8610d2
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-15 11:08:53 +0000

    Add basic docstrings

--- inferi/variables.py
+++ inferi/variables.py
@@ -1,7 +1,17 @@
-class Variable(list):
+"""Contains the basic Variable class and its methods."""
 
-    def __init__(self, *args, name=None, **kwargs):
-        list.__init__(self, [*args], **kwargs)
+class Variable(list):
+    """A Variable represents a set of measurements made on a collection of
+    entities, such as the heights of a group of football players, or the
+    favourite colours of a class of school children.
+
+    It is basically an extended list.
+
+    :param \*values: The measurements, as positional arguments.
+    :param str name: The name of this measurement."""
+
+    def __init__(self, *values, name=None, **kwargs):
+        list.__init__(self, [*values], **kwargs)
 
         if not isinstance(name, str) and name is not None:
             raise TypeError("Variable name must be str, not '%s'" % str(name))
