06d295803007b513ada9adbb77f725cd6995a664
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-22 12:45:57 +0000

    Start 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="inferi",
- version="0.1.0",
+ version="0.2.0",
  description="A statistics library.",
  url="https://inferi.samireland.com",
  author="Sam Ireland",
