83e0bcfdf1a814ee3b5873df8e8c5d42b310904d
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-13 16:05:55 +0100

    Add support for multiple columns in Datasets

--- inferi/datasets.py
+++ inferi/datasets.py
@@ -5,7 +5,12 @@ class Dataset:
     def __init__(self, *rows):
         """Represents a table of data."""
 
-        self._rows = [[row] for row in rows]
+        try:
+            self._rows = [[v for v in row] for row in rows]
+        except:
+            self._rows = [[row] for row in rows]
         self._x = list(range(len(rows)))
-        self._names = ["y1"]
+        self._names = [
+         "y{}".format(n + 1 if n else "") for n in range(len(self._rows[0]))
+        ]
         self._xname = "x"
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -7,5 +7,15 @@ class DatasetCreationTests(TestCase):
         dataset = Dataset(3, 5, 2, 4, 1, 3)
         self.assertEqual(dataset._rows, [[3], [5], [2], [4], [1], [3]])
         self.assertEqual(dataset._x, [0, 1, 2, 3, 4, 5])
-        self.assertEqual(dataset._names, ["y1"])
+        self.assertEqual(dataset._names, ["y"])
+        self.assertEqual(dataset._xname, "x")
+
+
+    def test_can_create_dataset_with_multiple_columns(self):
+        dataset = Dataset([3, 45], [5, 43], [2, 21], [4, 55], [1, 34], [3, 34])
+        self.assertEqual(
+         dataset._rows, [[3, 45], [5, 43], [2, 21], [4, 55], [1, 34], [3, 34]]
+        )
+        self.assertEqual(dataset._x, [0, 1, 2, 3, 4, 5])
+        self.assertEqual(dataset._names, ["y", "y2"])
         self.assertEqual(dataset._xname, "x")
