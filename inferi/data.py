0f6c39550fc86ed89ac06e0b4c4bf1b08947a027
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-21 22:18:51 +0100

    Add ability to provide x values to Data

--- inferi/data.py
+++ inferi/data.py
@@ -6,4 +6,10 @@ class Data:
     :param \*values: The values that make up the data."""
 
     def __init__(self, *values):
-        self._values = [[index, value] for index, value in enumerate(values)]
+        try:
+            if str in [type(v) for v in values]: raise TypeError
+            self._values = [[index, value] for index, value in values]
+        except ValueError:
+            raise ValueError("Iterable values can only be of length 2")
+        except TypeError:
+            self._values = [[index, value] for index, value in enumerate(values)]
--- tests/test_data.py
+++ tests/test_data.py
@@ -4,8 +4,22 @@ from inferi.data import Data
 class DataCreationTests(TestCase):
 
     def test_data_creation_with_values(self):
-        data = Data(23, 5, 5, 18, 17, 20)
-        self.assertEqual(
-         data._values,
-         [[0, 23], [1, 5], [2, 5], [3, 18], [4, 17], [5, 20]]
-        )
+        data = Data(23, 5, 5)
+        self.assertEqual(data._values, [[0, 23], [1, 5], [2, 5]])
+
+
+    def test_data_creation_with_x_values(self):
+        data = Data(("K", 23), ("C", 5), ("A", 5))
+        self.assertEqual(data._values, [["K", 23], ["C", 5], ["A", 5]])
+
+
+    def test_xy_pairs_must_be_len_2(self):
+        with self.assertRaises(ValueError):
+            Data(("K", 23), ("C", 5), ("A", 5, 6))
+        with self.assertRaises(ValueError):
+            Data(("K", 23), ("C", 5), ("A",))
+
+
+    def test_strings_dont_count_as_iterables(self):
+        data = Data("Sam", "Jonas", "Sally")
+        self.assertEqual(data._values, [[0, "Sam"], [1, "Jonas"], [2, "Sally"]])
