b8a9ff572f58c348914f653d4fecd916c1009521
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-21 22:22:56 +0100

    Add name to Data

--- inferi/data.py
+++ inferi/data.py
@@ -5,7 +5,7 @@ class Data:
 
     :param \*values: The values that make up the data."""
 
-    def __init__(self, *values):
+    def __init__(self, *values, name="y"):
         try:
             if str in [type(v) for v in values]: raise TypeError
             self._values = [[index, value] for index, value in values]
@@ -13,3 +13,6 @@ class Data:
             raise ValueError("Iterable values can only be of length 2")
         except TypeError:
             self._values = [[index, value] for index, value in enumerate(values)]
+        if not isinstance(name, str):
+            raise TypeError("name '{}' is not a str".format(name))
+        self._name = name
--- tests/test_data.py
+++ tests/test_data.py
@@ -6,6 +6,7 @@ class DataCreationTests(TestCase):
     def test_data_creation_with_values(self):
         data = Data(23, 5, 5)
         self.assertEqual(data._values, [[0, 23], [1, 5], [2, 5]])
+        self.assertEqual(data._name, "y")
 
 
     def test_data_creation_with_x_values(self):
@@ -23,3 +24,14 @@ class DataCreationTests(TestCase):
     def test_strings_dont_count_as_iterables(self):
         data = Data("Sam", "Jonas", "Sally")
         self.assertEqual(data._values, [[0, "Sam"], [1, "Jonas"], [2, "Sally"]])
+
+
+    def test_can_provide_name(self):
+        data = Data(23, 5, 5, name="heights")
+        self.assertEqual(data._values, [[0, 23], [1, 5], [2, 5]])
+        self.assertEqual(data._name, "heights")
+
+
+    def test_name_must_be_str(self):
+        with self.assertRaises(TypeError):
+            Data(23, 5, 5, name=100)
