114728d6a0f0233d5f6f1a0bd66079a646820fb3
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-27 14:38:55 +0100

    Add docstring for Data

--- inferi/data.py
+++ inferi/data.py
@@ -3,9 +3,26 @@
 from .exceptions import DuplicateXError
 
 class Data:
-    """Represents a list of measurements.
+    """Represents a list of measurements, as x-value, y-value pairs. Generally
+    this class would not be instantiated directly, but is here to act as a
+    parent class for others.
 
-    :param \*values: The values that make up the data."""
+    You can provide a list (or tuple, or set - any collection other than string)
+    of values, and these will become the y values of the data, with the input x
+    values being 0-based indices. Alternatively, you can provide a list of
+    length-2 lists (or tuples etc.) and *these* will be interpreted as xy pairs.
+
+    A ``Data`` object is a container of its y values, is iterable (stepping
+    through it will step through its y values), and its x values are accessed
+    using square brackets like a dictionary, with the corresponding x-value
+    being the key.
+
+    You can delete a value using ``del data[x_value]`` notation.
+
+    :param \*values: The values that make up the data.
+    :param str name: The name of the y-values.
+    :param str xname: The name of the x-values."""
+
 
     def __init__(self, *values, name="y", xname="x"):
         try:
