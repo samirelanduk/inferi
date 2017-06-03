1bb23006fd2b0b13fa8b1c46dec572dd90119022
Sam Ireland <sam.ireland.uk@gmail.com>
2017-06-03 16:42:04 +0100

    Add Value error

--- inferi/values.py
+++ inferi/values.py
@@ -4,10 +4,17 @@ class Value:
     """A Value represents a numerical measurement of some kind, with its
     associated uncertainty/error.
 
-    :param value: The value."""
+    For example a value of 23 ± 0.2 would be ``Value(23, 0.2)``
 
-    def __init__(self, value):
+    :param value: The value.
+    :param error: The uncertainty associated with the value. By default this is\
+    zero.
+    :raises TypeError: if either the value or its error is not numeric."""
+
+    def __init__(self, value, error=0):
         if not isinstance(value, (int, float)) or isinstance(value, bool):
-            return TypeError("{} is not an int or a float".format(value))
+            return TypeError("value {} is not an int or a float".format(value))
+        if not isinstance(error, (int, float)) or isinstance(error, bool):
+            return TypeError("error {} is not an int or a float".format(error))
         self._value = value
-        self._error = 0
+        self._error = error
--- tests/test_values.py
+++ tests/test_values.py
@@ -16,3 +16,18 @@ class ValueCreationTests(TestCase):
             Value("100")
         with self.assertRaises(TypeError):
             Value(True)
+
+
+    def test_can_supply_error(self):
+        val = Value(23, 0.03)
+        self.assertEqual(val._value, 23)
+        self.assertEqual(val._error, 0.03)
+
+
+    def test_error_requires_numbers(self):
+        Value(23, 1)
+        Value(23, 0.5)
+        with self.assertRaises(TypeError):
+            Value(23, "100")
+        with self.assertRaises(TypeError):
+            Value(23, True)
